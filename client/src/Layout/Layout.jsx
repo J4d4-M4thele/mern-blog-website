a49ced206e521fb6062c85cb3b210d3b29a45fba
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2026-09-18 17:45:22 -0700

    updating layout

---
+++ client/src/Layout/Layout.jsx
@@ -0,0 +1,20 @@
+import AppSidebar from "@/components/AppSidebar";
+import Footer from "@/components/Footer";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <SidebarProvider>
+      <Topbar />
+      <AppSidebar />
+      <main>
+        <Outlet />
+        <Footer />
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default Layout;
