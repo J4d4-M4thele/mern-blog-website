bdd0a963abf55acde34a8ad0932675f6d466cd94
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2026-09-22 13:09:05 -0700

    update signin component

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,69 +1,95 @@
 import React from "react";
-import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
-import {z} from "zod";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {useForm} from "react-hook-form";
+import { Card } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {RouteSignUp} from "@/helpers/RouteName";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
+  const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, "Password must be at least 8 characters long."),
+  });
 
-    const formSchema = z.object({
-        email: z.string().email(),
-        password: z.string().min(8, 'Password must be at least 8 characters long.')
-    })
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    const form = useForm({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            email: '',
-            password: '',
-        },
-    })
-
-    function onSubmit(values) {
-        console.log(values)
-    }
+  function onSubmit(values) {
+    console.log(values);
+  }
 
   return (
     <div className="flex justify-center items-center h-screen w-screen">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <div className="mb-3">
-            <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter your email address" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )} 
-          />
-          </div>
-          <div className="mb-3">
-            <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter your password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )} 
-          />
-          </div>
-          <Button type="submit" className="w-full">
-            Sign In
-          </Button>
-        </form>
-      </Form>
+      <Card className="w-[400px] p-5">
+        <h1 className="text-2xl font-bold text-center mb-5">
+          Sign Into Your Account
+        </h1>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="mb-3">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your email address"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="mb-3">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="mt-5">
+              <Button type="submit" className="w-full">
+                Sign In
+              </Button>
+              <div className="mt-5 text-sm flex justify-center items-center gap-2">
+                <p>Don&apos;t have an account?</p>
+                <Link className="text-blue-500 hover:underline" to={RouteSignUp}>
+                  Sign Up
+                </Link>
+              </div>
+            </div>
+          </form>
+        </Form>
+      </Card>
     </div>
   );
 };
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SignUp = () => {
   return (
     <div className='flex justify-center items-center h-screen w-screen'>
-      
+      Sign Up
     </div>
   )
 }
